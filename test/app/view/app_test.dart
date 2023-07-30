9a0110b566e6a06c7fb41c9e9c27aca1c56272dd
Tuyen Ha <tuyen3962@gmail.com>
2023-07-30 20:59:56 +0700

    init

---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:demo_dio/app/app.dart';
+import 'package:demo_dio/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
